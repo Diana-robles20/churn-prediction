9b3998bb9a9664432619204d62ed17d714299a05
Sammy <168230830+sammywntr@users.noreply.github.com>
2026-05-13 22:00:01 -0600

    ML Engineer: Entrenamiento del Modelo

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
-if __name__ == "__main__": pass
+from trainer_model import train_and_save_model
+
+if __name__ == "__main__":
+    ''' Entrenamiento del Modelo '''
+    print("Iniciando fase de entrenamiento...")
+
+    metrics = train_and_save_model(X_train, y_train, X_test, y_test, config)
+
+    print("\n Estos son los resultados del modelo:")
+    for metric, value in metrics.items():
+        print(f"{metric}: {value: .4f}")
