54f48262635ea4deff3776e32cc3f1d9927e181c
Sammy <168230830+sammywntr@users.noreply.github.com>
2026-05-14 11:54:32 -0600

    ML Engineer: Entrenamiento del Modelo

--- src/main.py
+++ src/main.py
@@ -2,10 +2,14 @@ from trainer_model import train_and_save_model
 
 if __name__ == "__main__":
     ''' Entrenamiento del Modelo '''
-    print("Iniciando fase de entrenamiento...")
+    print("\nIniciando competencia entre modelos y fase de entrenamiento...")
 
     metrics = train_and_save_model(X_train, y_train, X_test, y_test, config)
 
-    print("\n Estos son los resultados del modelo:")
+    print("\nEstos son los resultados del modelo ganador:")
     for metric, value in metrics.items():
-        print(f"{metric}: {value: .4f}")
+        if metric == 'Modelo Ganador':
+            print(f"- {metric}: {value}")
+
+        else:
+            print(f"- {metric}: {value: .4f}")
